629aa6d37686f01f0bae6ca319ac8245c7a3064b
20037834 <20037834@myrp.edu.sg>
2021-05-07 10:15:08 +0800

    Linked the variable to the UI

--- app/src/main/java/sg/edu/rp/c346/id20037834/p03_demo_simple_click/MainActivity.java
+++ app/src/main/java/sg/edu/rp/c346/id20037834/p03_demo_simple_click/MainActivity.java
@@ -3,12 +3,22 @@ package sg.edu.rp.c346.id20037834.p03_demo_simple_click;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView tvDisplay;
+    Button buttonDisplay;
+    EditText editInput;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        tvDisplay = findViewById(R.id.textViewDisplay);
+        buttonDisplay = findViewById(R.id.buttonDisplay);
+        editInput = findViewById(R.id.editTextInput);
     }
 }
